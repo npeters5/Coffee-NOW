c13bc9aa9e9fe2bffba1c0661918fb2d17b874e9
Nora Peters <norapeters5@gmail.com>
2018-07-17 14:35:00 -0700

    successfully tracking text field changes

--- app/screens/SignIn.js
+++ app/screens/SignIn.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import { View } from "react-native";
 import { Card, Button, FormLabel, FormInput } from "react-native-elements";
 import { onSignIn } from "../auth";
 
-export default ({ navigation }) => (
-  <View style={{ paddingVertical: 20 }}>
-    <Card>
-      <FormLabel>Email</FormLabel>
-      <FormInput placeholder="Email address..." />
-      <FormLabel>Password</FormLabel>
-      <FormInput secureTextEntry placeholder="Password..." />
+class SignIn extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
 
-      <Button
-        buttonStyle={{ marginTop: 20 }}
-        backgroundColor="#03A9F4"
-        title="SIGN IN"
-        onPress={() => {
-          onSignIn().then(() => navigation.navigate("SignedIn"));
-        }}
-      />
-    </Card>
-  </View>
-);
+  onFieldChange = fieldName => text => {
+    this.setState({
+      [fieldName]: text
+    });
+  };
+
+  render() {
+    const { navigation } = this.props;
+    console.log(this.state);
+
+    return (
+      <View style={{ paddingVertical: 20 }}>
+        <Card>
+          <FormLabel>Email</FormLabel>
+          <FormInput
+            placeholder="Email address..."
+            value={this.state.email}
+            onChangeText={this.onFieldChange("email")}
+          />
+
+          <FormLabel>Password</FormLabel>
+          <FormInput
+            secureTextEntry
+            placeholder="Password..."
+            value={this.state.password}
+            onChangeText={this.onFieldChange("password")}
+          />
+
+          <Button
+            buttonStyle={{ marginTop: 20 }}
+            backgroundColor="#03A9F4"
+            title="SIGN IN"
+            onPress={() => {
+              onSignIn().then(() => navigation.navigate("SignedIn"));
+            }}
+          />
+        </Card>
+      </View>
+    );
+  }
+}
+
+export default SignIn;
